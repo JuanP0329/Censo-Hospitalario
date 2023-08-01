e14dd96f4f42e2d3587b54494830ff8998833bbe
JuanP0329 <jpbenjumeago@cesde.net>
2023-08-01 17:09:46 -0500

    se traduce la aplicacion del proyecto doctores

--- doctores/admin.py
+++ doctores/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from .models import Doctor
+from django.utils.safestring import mark_safe
+
+from .forms import DoctorForm
+from .models import Doctor, Specialty
+
 
 # Register your models here.
 
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'cedula', 'especialidad', 'correo']
-    list_display_links = ['nombre', 'cedula', 'especialidad', 'correo']
+    form = DoctorForm
+    list_display = ['identification', 'photo', 'name', 'last_name', 'email', 'specialty']
+    list_display_links = list_display
+    fields = ['identification', 'photo', 'photo_tag', 'name', 'last_name', 'email', 'specialty']
+    readonly_fields = ['photo_tag']
+
+    @staticmethod
+    def photo_tag(doctor):
+        if doctor.photo:
+            return mark_safe('<img src="%s" width="300"/>' % doctor.photo.url)
+        else:
+            return mark_safe('-')
+
+
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = list_display
--- doctores/models.py
+++ doctores/models.py
@@ -1,13 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Doctor(models.Model):
-    nombre = models.CharField(max_length=200)
-    cedula = models.IntegerField(primary_key=True)
-    especialidad = models.CharField(max_length=200)
-    correo = models.EmailField(max_length=200)
+    identification = models.IntegerField(primary_key=True)
+    photo = models.ImageField(upload_to='pictures', blank=True, null=True)
+    name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    specialty = models.ForeignKey('Specialty', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Doctor'
+        verbose_name_plural = 'Doctores'
+        ordering = ['identification']
 
     def __str__(self):
-        return f'{self.nombre}'
+        return f'{self.name} {self.last_name}'
+
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Specialty'
+        verbose_name_plural = 'Specialties'
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
